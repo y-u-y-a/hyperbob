292e49107f0e7f097564b4d2e6bd767ba1818624
y-u-y-a <47471299+y-u-y-a@users.noreply.github.com>
2023-04-11 20:07:09 +0900

    feat: send transaction

--- src/pages/Account/components/transaction/transaction.tsx
+++ src/pages/Account/components/transaction/transaction.tsx
@@ -1,13 +1,12 @@
-import {
-  Button,
-  CardActions,
-  CardContent,
-  Stack,
-  Typography,
-} from '@mui/material';
-import React from 'react';
-import { EthersTransactionRequest } from '../../../Background/services/provider-bridge';
+import { Box, Button, Input, Stack } from '@mui/material';
+import React, { useState } from 'react';
 import { TransactionComponentProps } from '../types';
+import { useBackgroundDispatch } from '../../../App/hooks';
+import {
+  sendTransactionRequest,
+  setModifyTransactionsRequest,
+} from '../../../Background/redux-slices/transactions';
+import { useNavigate } from 'react-router-dom';
 
 // NOTE: ダミーコンポーネント本体
 const Transaction = ({
@@ -15,44 +14,70 @@ const Transaction = ({
   onComplete,
   onReject,
 }: TransactionComponentProps) => {
+  const backgroundDispatch = useBackgroundDispatch();
+  const navigate = useNavigate();
+  //
+  const [gkAddress, setGkAddress] = useState('');
+  const [tokenAddress, setTokenAddress] = useState('');
+  const [val, setVal] = useState('');
   return (
-    <>
-      <CardContent>
-        <Typography variant="h3" gutterBottom>
-          Dummy Account Component
-        </Typography>
-        <Typography variant="body1" color="text.secondary">
-          You can show as many steps as you want in this dummy component. You
-          need to call the function <b>onComplete</b> passed as a props to this
-          component. <br />
-          <br />
-          The function takes a modifiedTransactions & context as a parameter,
-          the context will be passed to your AccountApi when creating a new
-          account. While modifiedTransactions will be agreed upon by the user.
-          <br />
-          This Component is defined in exported in{' '}
-          <pre>
-            trampoline/src/pages/Account/components/transaction/index.ts
-          </pre>
-        </Typography>
-      </CardContent>
-      <CardActions sx={{ pl: 4, pr: 4, width: '100%' }}>
-        <Stack spacing={2} sx={{ width: '100%' }}>
+    <Box display="flex" flexDirection="column" justifyContent="center">
+      <h3 style={{ margin: '20px auto', fontSize: '20px', fontWeight: 'bold' }}>
+        Enter a destination address
+      </h3>
+      <Box marginBottom="20px">
+        <Stack spacing={2} style={{ width: '80%', margin: '0 auto 20px' }}>
+          <Input
+            value={gkAddress}
+            name="gk_address"
+            placeholder="gk_addressを入力してください"
+            onChange={(e) => setGkAddress(e.target.value)}
+            sx={{ paddingLeft: '10px', paddingRight: '10px' }}
+          />
+          <Input
+            value={tokenAddress}
+            name="token_address"
+            placeholder="token_addressを入力してください"
+            onChange={(e) => setTokenAddress(e.target.value)}
+            sx={{ paddingLeft: '10px', paddingRight: '10px' }}
+          />
+          <Input
+            value={val}
+            name="value"
+            placeholder="valueを入力してください"
+            onChange={(e) => setVal(e.target.value)}
+            sx={{ paddingLeft: '10px', paddingRight: '10px' }}
+          />
+        </Stack>
+        <Box display="flex" justifyContent="center">
           <Button
             size="large"
             variant="contained"
-            onClick={() => {
-              // TODO: 新しいトランザクションを作成&storeにセットする
-              // TODO: ここでstoreにセットする方法を解明する
-              onComplete(transaction, undefined);
+            onClick={async () => {
+              try {
+                // TODO: 新しいトランザクションを作成
+                const dummy = Array.prototype.slice.call({ 0: 'a', length: 1 });
+                transaction.data = dummy;
+                await backgroundDispatch(
+                  // transactionRequestのstateを変更する
+                  sendTransactionRequest({
+                    transactionRequest: transaction,
+                    origin: 'https://yahoo.co.jp',
+                  })
+                );
+                onComplete(transaction, undefined);
+                console.log({ transaction });
+                navigate('/complete');
+              } catch (error) {
+                console.log({ error });
+              }
             }}
           >
             Continue
           </Button>
-        </Stack>
-        <div>{JSON.stringify(transaction)}</div>
-      </CardActions>
-    </>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
