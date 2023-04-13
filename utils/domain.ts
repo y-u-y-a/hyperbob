a813d699a476d5562271a3e9451fb9e52d3052de
figboy9 <figboy9@tuta.io>
2023-04-13 09:47:51 +0900

    アドレスなどutil追加

---
+++ utils/domain.ts
@@ -0,0 +1,4 @@
+export const domain = {
+    goerli: 1,
+    sepolia: 11155111
+}
---
+++ utils/gas.ts
@@ -0,0 +1,7 @@
+
+export const gas = {
+    GAS_LIMIT: 500000,
+    GAS_AMOUNT: 500000,
+    DEST_GAS_AMOUNT: 700000,
+ 
+ }
