0423b9a90b10b7cba8fd715ea7fb03c1bca45d4a
figboy9 <figboy9@tuta.io>
2023-04-13 09:51:36 +0900

    依存関係削除

--- utils/index.ts
+++ utils/index.ts
@@ -1,18 +1,4 @@
 // export * from "./abi"
-export * from "./address"
-export * from "./domain"
-export * from "./gas"
-
-import * as HypERC20Artifacts from "./abi/HypERC20.json"
-import * as HypERC20CollateralArtifacts from "./abi/HypERC20Collateral.json"
-import * as MockReceiverArtifacts from "./abi/MockERC777Receiver.json"
-import * as wethAbi from "./abi/weth.json"
-import * as ERC1820Abi from "./abi/ERC1820.json"
-
-export const abi = {
-    hypERC20: HypERC20Artifacts.abi,
-    hypERC20Collateral: HypERC20CollateralArtifacts.abi,
-    mockReceiver: MockReceiverArtifacts.abi,
-    weth: wethAbi.abi,
-    erc1820: ERC1820Abi.abi
-}
+export * from './address';
+export * from './domain';
+export * from './gas';
