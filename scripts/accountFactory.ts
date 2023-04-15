66a6fe8a16efb019f6dc3d197e23268121d401da
figboy9 <figboy9@tuta.io>
2023-04-16 03:37:19 +0900

    coount factory deterministic deploy

--- scripts/accountFactory.ts
+++ scripts/accountFactory.ts
@@ -5,27 +5,32 @@ import {} from '../src/pages/Account';
 import { ethers } from 'hardhat';
 import { AccountFactory__factory } from '../src/pages/Account/account-api/typechain-types';
 import { address } from '../src/utils/address';
+import { DeterministicDeployer } from '@account-abstraction/sdk';
+import { providers } from 'ethers';
 
 async function main() {
   const [signer] = await ethers.getSigners();
-
   const factory = new AccountFactory__factory(signer);
-  const accountFactory = await factory.deploy(
-    '0x0576a174D229E3cFA37253523E645A78A0C91B57'
-  );
 
-  console.log('AccountFactory Address: ', accountFactory.address);
+  const dep = new DeterministicDeployer(ethers.provider);
+  const accountFactoryAddr = await dep.deterministicDeploy(factory, 0, [
+    '0x0576a174D229E3cFA37253523E645A78A0C91B57',
+  ]);
+
+  const accountFactroy = factory.attach(accountFactoryAddr);
+
+  console.log('AccountFactory Address: ', accountFactoryAddr);
   console.log(
     'Account Imp Address: ',
-    await accountFactory.callStatic.accountImplementation()
+    await accountFactroy.callStatic.accountImplementation()
   );
 
   const { chainId } = await ethers.provider.getNetwork();
   if (chainId == 5) {
-    address.goerli.accountFactory = accountFactory.address;
+    address.goerli.accountFactory = accountFactroy.address;
   }
   if (chainId == 11155111) {
-    address.sepolia.accountFactory = accountFactory.address;
+    address.sepolia.accountFactory = accountFactroy.address;
   }
 }
 
