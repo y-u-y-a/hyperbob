71554973414fb56ac8a7f9730f5b8b65f841a255
figboy9 <figboy9@tuta.io>
2023-04-15 19:23:01 +0900

    fix resources deploy

--- utils/address.ts
+++ utils/address.ts
@@ -11,8 +11,9 @@ export const address = {
     BOB: '0x97a4ab97028466FE67F18A6cd67559BAABE391b8',
     // hyperBOB
     accountImp: '0xeb3d8191d134B40fF093eB9FA56adEdE3389d6a6',
-    accountFactory: '0x95751b91b1d34537cd8a208d9b39093538d9d510',
-    hypERCCollateral: '0xd06532148869ba2fdb1af29c79ba79002a833be0',
+    accountFactory: '0xE8D3AF8dE4B0a7Cdaf73B041dE0166B505DABcb3',
+    // old: 0x95751b91b1d34537cd8a208d9b39093538d9d510
+    hypERCCollateral: '0xCc997E02991b8Ac033296a03117e55Ac4E6C32C9',
 
     // Uniswap
     router: '0x4648a43B2C14Da09FdF82B161150d3F634f40491',
@@ -30,8 +31,8 @@ export const address = {
 
     // hyperBOB
     accountImp: '0x3e809ab2E61A6734Fea18E3F781a7c4b87c8291E',
-    accountFactory: '0x95751b91b1d34537cd8a208d9b39093538d9d510',
-    hypERC: '0x0da95935ad5499a920cf179db3e5bf86d6c681e7',
+    accountFactory: '0x95ba6f70a1F8ac82ED20714635052656c9F44FfA',
+    hypERC: '0xfB1C4Ad3af37084c0419bC6E76d7e6b0cf389339',
   },
   common: {
     entryPoint: '0x0576a174D229E3cFA37253523E645A78A0C91B57',
